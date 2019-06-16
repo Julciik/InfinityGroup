b51d90540a075c3a38a47b3419dcf6e6993a3dc3
Julia Kolinko <jul.kolinko@gmail.com>
2019-06-16 12:59:18 +0200

    Fixed scroll menu

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const logo = document.querySelector(".logo img");
 const media = window.matchMedia("(min-width: 900px)")
 const cookieWin = document.querySelector(".cookies");
 const cookieBtn = document.querySelector(".cookies button");
+const navMenu = document.querySelector(".menu");
+const navUl = document.querySelector(".menu ul");
 
 
 const scroll = () => {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 && media.matches) {
         scrollMenu.style.display = "block";
         logo.style.marginTop = "10em";
+        navMenu.style.position = "fixed";
+        navMenu.style.paddingLeft = "100px";
     } else {
         scrollMenu.style.display = "none";
         logo.style.marginTop = "0";
+        navMenu.style.position = "static";
+        navMenu.style.paddingLeft = "0";
     }
 };
 
