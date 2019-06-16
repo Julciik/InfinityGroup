1f6177abce0d103665b106298e1aae52fc8c3c60
Julia Kolinko <jul.kolinko@gmail.com>
2019-06-16 15:59:10 +0200

    Jump to arrows

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const media = window.matchMedia("(min-width: 900px)")
 const cookieWin = document.querySelector(".cookies");
 const cookieBtn = document.querySelector(".cookies button");
 const navMenu = document.querySelector(".menu");
-const navUl = document.querySelector(".menu ul");
+const arrows = document.querySelector(".arrows");
+const upArr = document.querySelector(".upArrow");
+const downArr = document.querySelector(".downArrow");
+const missionPosition = document.getElementById("mission").offsetTop;
+const clientsPosition = document.getElementById("clients").offsetTop;
+const productsPosition = document.getElementById("products").offsetTop;
+const mapPosition = document.getElementById("contactMap").offsetTop;
+const contactPosition = document.getElementById("contact").offsetTop;
+const comments = document.querySelectorAll(".comment");
 
-
-const scroll = () => {
+const scrollFixedMenu = () => {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 && media.matches) {
         scrollMenu.style.display = "block";
         logo.style.marginTop = "10em";
@@ -22,6 +29,49 @@ const scroll = () => {
     }
 };
 
+window.addEventListener("scroll", scrollFixedMenu);
+
+window.addEventListener("scroll", () => {
+    if (this.scrollY >= arrows.offsetTop) {
+        arrows.style.position = "fixed";
+    } else {
+        arrows.style.position = "absolute";
+    }
+});
+
+const jumpToSectionUp = () => {
+    if (window.scrollY >= missionPosition && window.scrollY < clientsPosition) {
+        upArr.href = "#top";
+    } else if (window.scrollY >= clientsPosition && window.scrollY < productsPosition) {
+        upArr.href = "#mission";
+    } else if (window.scrollY >= productsPosition && window.scrollY < mapPosition) {
+        upArr.href = "#clients";
+    } else if (window.scrollY >= mapPosition && window.scrollY < contactPosition) {
+        upArr.href = "#products";
+    } else if (window.scrollY >= contactPosition) {
+        upArr.href = "#mapContact";
+    }
+};
+
+const jumpToSectionDown = () => {
+    if (window.scrollY >= missionPosition && window.scrollY < clientsPosition) {
+        downArr.href = "#clients";
+    } else if (window.scrollY >= clientsPosition && window.scrollY < productsPosition) {
+        downArr.href = "#products";
+    } else if (window.scrollY >= productsPosition && window.scrollY < mapPosition) {
+        downArr.href = "#contactMap";
+    } else if (window.scrollY >= mapPosition && window.scrollY < contactPosition) {
+        downArr.href = "#contact";
+    } else if (window.scrollY >= contactPosition) {
+        downArr.href = "#bottom";
+    }
+};
+
+document.addEventListener("ready", jumpToSectionDown);
+window.addEventListener("scroll", jumpToSectionUp);
+window.addEventListener("scroll", jumpToSectionDown);
+upArr.addEventListener("click", jumpToSectionUp);
+downArr.addEventListener("click", jumpToSectionDown);
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("rotate");
@@ -30,8 +80,6 @@ hamburger.addEventListener("click", () => {
     } else hamburger.classList.add("active")
 
 });
-
-window.addEventListener("scroll", scroll);
 
 cookieBtn.addEventListener("click", () => {
     cookieWin.style.visibility = "hidden";
